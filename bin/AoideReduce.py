5bf7fadcd8422110c0a36c8ea9bd671498440a83
Grant Tremblay <granttremblay@gmail.com>
2018-06-02 23:21:55 -0400

    update

--- bin/AoideReduce.py
+++ bin/AoideReduce.py
@@ -29,10 +29,10 @@ def main():
     print("_________________________   Setting Up    _______________________\n")
 
     raw_data_dir = os.path.abspath(args.rawdata)
-    reduction_dir = os.path.join(raw_data_dir, '../reduction')
+    reduction_dir = os.path.abspath(os.path.join(raw_data_dir, '../reduction'))
     esorex_path = spawn.find_executable("esorex")
 
-    print("Raw data directory set to {}".format(args.rawdata))
+    print("Raw data directory set to {}".format(raw_data_dir))
     print("Using {} processor cores for reduction.".format(args.cores))
 
     if os.path.exists(reduction_dir):
