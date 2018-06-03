decb25905c5e36bfbcb6dea80df227e14b0ddbd1
Grant Tremblay <granttremblay@gmail.com>
2018-06-02 22:30:11 -0400

    adding identifier script

---
+++ bin/AoideID.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env/python
+
+import glob
+from astropy.io import fits
+
+rawfiles = glob.glob("*.fits.fz")
+
+for file in rawfiles:
+    print("{} | DPR TYPE {} | TARGNAME is {} | EXPTIME is {}s".format(file, fits.getval(file, "ESO DPR TYPE"), fits.getval(file, "OBJECT"), fits.getval(file, "EXPTIME")))
